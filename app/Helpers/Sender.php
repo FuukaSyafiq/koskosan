b24be787a1d01c24f1bde4122dc6024db1095947
syafiqparadisam <syafiqparadisam@gmail.com>
2026-04-05 11:19:38 +0700

    feat: uncomment SendToWhatsapp function

--- app/Helpers/Sender.php
+++ app/Helpers/Sender.php
@@ -6,20 +6,20 @@ use Illuminate\Support\Facades\Http;
 
 class Sender
 {
-	public static function SendToWhatsapp($noTelp, $text)
-	{
-		try {
-			// $apiUrl = env("API_WHATSAPP_URL");
-			// // dd($apiUrl);
-			// Http::asForm()->post($apiUrl, [
-			// 	'no_telepon' => $noTelp,
-			// 	'isi_pesan' => $text,
-			// 	'app_name' => 'ojt_kost'
-			// ]);
-			
-		} catch (\Exception $e) {
-			dd($e->getMessage());
-			throw $e;
-		}
-	}
+    public static function SendToWhatsapp($noTelp, $text)
+    {
+        try {
+            $apiUrl = env('API_WHATSAPP_URL');
+            // dd($apiUrl);
+            Http::asForm()->post($apiUrl, [
+                'no_telepon' => $noTelp,
+                'isi_pesan' => $text,
+                'app_name' => 'ojt_kost',
+            ]);
+
+        } catch (\Exception $e) {
+            dd($e->getMessage());
+            throw $e;
+        }
+    }
 }
